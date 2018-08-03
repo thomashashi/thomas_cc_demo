f1a14f36d986ae78bf39765f89ecf24ae9d4e9b3
Thomas Kula <thomas@hashicorp.com>
2018-08-03 11:38:21 -0400

    Add sleeps to install_mongodb.sh
    I'm not sure *why* I had to do it, but if I didn't sprinkle sleeps
    throughout this file dpkg would complain about something else holding
    the lock. :shrug:

--- packer/files/install_mongodb.sh
+++ packer/files/install_mongodb.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 
 # install mongodb
+echo "### apt-key adv"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+sleep 5
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+sleep 5
+echo "### apt-get update"
 sudo apt-get update
+sleep 5
 sudo apt-get install -y -qq mongodb-org 
+sleep 5
 
 sudo systemctl enable mongod
 sudo systemctl start mongod
