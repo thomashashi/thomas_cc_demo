5baef1a99c289dc0397e112c0c85b6bb773ac93f
Thomas Kula <thomas@hashicorp.com>
2018-08-07 11:17:04 -0400

    Add sleeps to files/install_node.sh
    Much like in f1a14f36d986ae78bf39765f89ecf24ae9d4e9b3 I have
    *no* *idea* why this fixes things, but it does :shrug:

--- packer/files/install_node.sh
+++ packer/files/install_node.sh
@@ -2,7 +2,9 @@
 
 # install Node
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sleep 15
 sudo apt-get install -y nodejs
+sleep 15
 
 # install the listing service app
 mkdir /home/ubuntu/src
