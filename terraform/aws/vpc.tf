5506ffd2cd0ef02fb98d2ecdce58302c79937e0f
Thomas Kula <thomas@hashicorp.com>
2018-08-20 11:36:28 -0400

    Clean out Terraform output by removing things we don't need

--- terraform/aws/vpc.tf
+++ terraform/aws/vpc.tf
@@ -10,11 +10,3 @@ data "aws_subnet" "default" {
     count = "${length(data.aws_subnet_ids.default.ids)}"
     id    = "${data.aws_subnet_ids.default.ids[count.index]}"
 }
-
-output "aws_subnet_ids" {
-    value = ["${data.aws_subnet_ids.default.ids}"]
-}
-
-output "aws_cidr_blocks" {
-    value = ["${data.aws_subnet.default.*.cidr_block}"]
-}
