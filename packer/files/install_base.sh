8114de0c5e97fa17f22b8a139b50f33d2b8d5d14
Thomas Kula <thomas@hashicorp.com>
2018-11-24 10:20:09 -0500

    Add reinvent user to base image

--- packer/files/install_base.sh
+++ packer/files/install_base.sh
@@ -22,5 +22,12 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl disable consul_enable_acl.service
 
+echo "Adding reinvent user"
+sudo adduser --disabled-password --gecos "reInvent User" reinvent
+sudo install -d -o reinvent -g reinvent -m 700 /home/reinvent/.ssh
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3KPO8/w3pGuCGFoE1s8YD9EokD+zk6gDzjXbKQL9Ll AWS re:Invent 2018 Consul Demo reinvent user" | sudo tee /home/reinvent/.ssh/authorized_keys > /dev/null
+sudo chown reinvent:reinvent /home/reinvent/.ssh/authorized_keys
+sudo chmod 600 /home/reinvent/.ssh/authorized_keys
+
 echo "Finished!"
 
