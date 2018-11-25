477f733345b9ef7aaca7923bfbfa3ecfa1099543
Thomas Kula <thomas@hashicorp.com>
2018-11-25 10:02:37 -0500

    Add tags to vpc peering connection

--- terraform/aws/link-dcs/main.tf
+++ terraform/aws/link-dcs/main.tf
@@ -15,4 +15,5 @@ data "terraform_remote_state" "west" {
 resource "aws_vpc_peering_connection" "east_west" {
     peer_vpc_id = "${data.terraform_remote_state.west.vpc_id}"
     vpc_id = "${data.terraform_remote_state.east.vpc_id}"
+    tags = "${merge(map("Name", "prod-east-west-link"), var.hashi_tags)}"
 }
