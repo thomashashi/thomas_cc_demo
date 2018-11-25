594f0c773877603e8fec020722e919a54581c1ad
Thomas Kula <thomas@hashicorp.com>
2018-11-25 18:08:17 -0500

    Attempt to do interpolation properly

--- terraform/aws/consul.tf
+++ terraform/aws/consul.tf
@@ -24,7 +24,7 @@ resource "aws_route53_record" "consul_a_records" {
     name = "consul${count.index}.${var.consul_dc}.reinventconsul.hashidemos.io"
     type = "A"
     ttl = "30"
-    records = ["${aws_instance.consul.*[count.index].public_ip}"]
+    records = ["${aws_instance.consul.*.public_ip[count.index]}"]
 }
 
 # Allow Consul Servers to call ec2:DescribeTags for Cloud AutoJoin
