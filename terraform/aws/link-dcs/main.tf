b128547405da4b354adf664567b80f9a6a1bc56d
Thomas Kula <thomas@hashicorp.com>
2018-11-25 10:00:30 -0500

    Add dc linkage

---
+++ terraform/aws/link-dcs/main.tf
@@ -0,0 +1,18 @@
+data "terraform_remote_state" "east" {
+    backend = "atlas"
+    config {
+	name = "kula/consul-reinvent-east"
+    }
+}
+
+data "terraform_remote_state" "west" {
+    backend = "atlas"
+    config {
+	name = "kula/consul-reinvent-west"
+    }
+}
+
+resource "aws_vpc_peering_connection" "east_west" {
+    peer_vpc_id = "${data.terraform_remote_state.west.vpc_id}"
+    vpc_id = "${data.terraform_remote_state.east.vpc_id}"
+}
