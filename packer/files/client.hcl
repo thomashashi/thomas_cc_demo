026dc9715187d6e4154d9b5ad7d089b7d3039fb5
Thomas Kula <thomas@hashicorp.com>
2018-11-25 17:00:11 -0500

    Remove ACL service remnants

--- packer/files/client.hcl
+++ packer/files/client.hcl
@@ -2,4 +2,3 @@ client_addr      = "0.0.0.0"
 data_dir         = "/opt/consul"
 log_level        = "INFO"
 server           = false
-acl_down_policy  = "extend-cache"
--- packer/files/install_base.sh
+++ packer/files/install_base.sh
@@ -5,23 +5,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get update -qq > /dev/null
 sudo DEBIAN_FRONTEND=noninteractive apt-get -qq upgrade > /dev/null
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq unzip wget jq python3-pip > /dev/null
 
-cat <<EOF | sudo tee /lib/systemd/system/consul_enable_acl.service
-[Unit]
-Description = Enable ACL system for consul agent
-Requires=consul.service
-After=consul.service
-
-[Service]
-Type=oneshot
-ExecStart=/etc/consul/enable_acl.sh
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo systemctl daemon-reload
-sudo systemctl disable consul_enable_acl.service
-
 echo "Adding reinvent user"
 sudo adduser --disabled-password --gecos "reInvent User" reinvent
 sudo install -d -o reinvent -g reinvent -m 700 /home/reinvent/.ssh
