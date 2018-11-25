442f4221deaa891c0c5520f0703bc32fd53bbe7b
Thomas Kula <thomas@hashicorp.com>
2018-11-25 10:37:58 -0500

    Use cross-region version of vpc peering

--- terraform/aws/link-dcs/main.tf
+++ terraform/aws/link-dcs/main.tf
@@ -12,9 +12,35 @@ data "terraform_remote_state" "west" {
     }
 }
 
-resource "aws_vpc_peering_connection" "east_west" {
-    peer_vpc_id = "${data.terraform_remote_state.west.vpc_id}"
-    peer_region = "us-west-2"
+provider "aws" {
+    region = "${data.terraform_remote_state.east.aws_region}"
+    alias = "east"
+}
+
+provider "aws" {
+    region = "${data.terraform_remote_state.west.aws_region}"
+    alias = "west"
+}
+
+data "aws_caller_identity" "west" {
+  provider = "aws.west"
+}
+
+resource "aws_vpc_peering_connection" "east" {
     vpc_id = "${data.terraform_remote_state.east.vpc_id}"
-    tags = "${merge(map("Name", "prod-east-west-link"), var.hashi_tags)}"
+    peer_vpc_id = "${data.terraform_remote_state.west.vpc_id}"
+    peer_owner_id = "${data.aws_caller_identity.west.account_id}"
+    peer_region = "us-west-2"
+    auto_accept = false
+
+    tags = "${merge(map("Name", "prod-east-west-link"), map("Side", "Requestor"), var.hashi_tags)}"
+}
+
+# Accepter's side of the connection.
+resource "aws_vpc_peering_connection_accepter" "west" {
+    provider                  = "aws.west"
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.west.id}"
+    auto_accept               = true
+
+    tags = "${merge(map("Name", "prod-east-west-link"), map("Side", "Acceptor"), var.hashi_tags)}"
 }
