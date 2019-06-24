f914e1c24a65970faf71d10daff5f269d75be4e2
Robert Peteuil <robert@peteuil.com>
2019-06-24 09:22:28 -0700

    add start of multi-region demo

---
+++ terraform/aws/demo-multi-region/main.tf
@@ -0,0 +1,78 @@
+# Consul Connect Demo Cluster - Single Region
+
+# Create MAIN Consul Connect cluster
+module "cluster_main" {
+  source = "../modules/consul-demo-cluster"
+
+  aws_region    = "${var.aws_region}"
+  consul_dc     = "${var.consul_dc}"
+  consul_acl_dc = "${var.consul_acl_dc}"
+
+  project_name     = "${var.project_name}"
+  top_level_domain = "${var.top_level_domain}"
+
+  route53_zone_id = "${var.route53_zone_id}"
+  ssh_key_name    = "${var.ssh_key_name}"
+  consul_lic      = "${var.consul_lic}"
+
+  hashi_tags = "${var.hashi_tags}"
+}
+
+# Configure MAIN Consul Cluster for Prepared Query
+provider "consul" {
+  address    = "${element(module.cluster_main.consul_servers, 0)}:8500"
+  datacenter = "${module.cluster_main.consul_dc}"
+}
+
+resource "consul_prepared_query" "product_service" {
+  datacenter   = "${module.cluster_main.consul_dc}"
+  name         = "product"
+  only_passing = true
+  connect      = true
+
+  service = "product"
+
+  failover {
+    datacenters = ["${module.cluster_main.consul_dc}"]
+  }
+}
+
+# Create ALTERNATE Consul Connect cluster
+module "cluster_alt" {
+  source = "../modules/consul-demo-cluster"
+
+  aws_region    = "${var.aws_region_alt}"
+  consul_dc     = "${var.consul_dc_alt}"
+  consul_acl_dc = "${module.cluster_main.consul_dc}"
+
+  project_name     = "${var.project_name}"
+  top_level_domain = "${var.top_level_domain}"
+
+  route53_zone_id = "${var.route53_zone_id}"
+  ssh_key_name    = "${var.ssh_key_name}"
+  consul_lic      = "${var.consul_lic}"
+
+  hashi_tags = "${var.hashi_tags}"
+}
+
+# Configure ALTERNATE Consul Cluster for Prepared Query
+provider "consul" {
+  address    = "${element(module.cluster_alt.consul_servers, 0)}:8500"
+  datacenter = "${module.cluster_alt.consul_dc}"
+}
+
+resource "consul_prepared_query" "product_service" {
+  datacenter   = "${module.cluster_alt.consul_dc}"
+  name         = "product"
+  only_passing = true
+  connect      = true
+
+  service = "product"
+
+  failover {
+    datacenters = ["${module.cluster_alt.consul_dc}"]
+  }
+}
+
+# TBD - Implement Code to Link VPCs - either via module or code here
+
---
+++ terraform/aws/demo-multi-region/outputs.tf
@@ -0,0 +1,67 @@
+# Outputs
+
+# Main Cluster Region
+output "main_region" {
+  value = "${module.cluster_main.aws_region}"
+}
+
+output "main_consul_lb" {
+  value = "${module.cluster_main.consul_lb}"
+}
+
+output "main_consul_servers" {
+  value = ["${module.cluster_main.consul_servers}"]
+}
+
+output "main_webclient_lb" {
+  value = "${module.cluster_main.webclient_lb}"
+}
+
+output "main_webclient_servers" {
+  value = ["${module.cluster_main.webclient_servers}"]
+}
+
+output "main_listing_api_servers" {
+  value = ["${module.cluster_main.listing_api_servers}"]
+}
+
+output "main_mongo_servers" {
+  value = ["${module.cluster_main.mongo_servers}"]
+}
+
+output "main_product_api_servers" {
+  value = ["${module.cluster_main.product_api_servers}"]
+}
+
+# Alternate Cluster Outputs
+output "main_region" {
+  value = "${module.cluster_alt.aws_region}"
+}
+
+output "alt_consul_lb" {
+  value = "${module.cluster_alt.consul_lb}"
+}
+
+output "alt_consul_servers" {
+  value = ["${module.cluster_alt.consul_servers}"]
+}
+
+output "alt_webclient_lb" {
+  value = "${module.cluster_alt.webclient_lb}"
+}
+
+output "alt_webclient_servers" {
+  value = ["${module.cluster_alt.webclient_servers}"]
+}
+
+output "alt_listing_api_servers" {
+  value = ["${module.cluster_alt.listing_api_servers}"]
+}
+
+output "alt_mongo_servers" {
+  value = ["${module.cluster_alt.mongo_servers}"]
+}
+
+output "alt_product_api_servers" {
+  value = ["${module.cluster_alt.product_api_servers}"]
+}
