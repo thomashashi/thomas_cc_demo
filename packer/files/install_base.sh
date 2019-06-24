86decf379c49661ec2014ac8189888c56c699e56
Robert Peteuil <robert@peteuil.com>
2019-06-24 13:13:33 -0700

    remove reInvent user, add delay to prevent crash

--- packer/files/install_base.sh
+++ packer/files/install_base.sh
@@ -5,12 +5,14 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get update -qq > /dev/null
 sudo DEBIAN_FRONTEND=noninteractive apt-get -qq upgrade > /dev/null
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq unzip wget jq python3-pip > /dev/null
 
-echo "Adding reinvent user"
-sudo adduser --disabled-password --gecos "reInvent User" reinvent
-sudo install -d -o reinvent -g reinvent -m 700 /home/reinvent/.ssh
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3KPO8/w3pGuCGFoE1s8YD9EokD+zk6gDzjXbKQL9Ll AWS re:Invent 2018 Consul Demo reinvent user" | sudo tee /home/reinvent/.ssh/authorized_keys > /dev/null
-sudo chown reinvent:reinvent /home/reinvent/.ssh/authorized_keys
-sudo chmod 600 /home/reinvent/.ssh/authorized_keys
+sleep 15
+
+# echo "Adding reinvent user"
+# sudo adduser --disabled-password --gecos "reInvent User" reinvent
+# sudo install -d -o reinvent -g reinvent -m 700 /home/reinvent/.ssh
+# echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3KPO8/w3pGuCGFoE1s8YD9EokD+zk6gDzjXbKQL9Ll AWS re:Invent 2018 Consul Demo reinvent user" | sudo tee /home/reinvent/.ssh/authorized_keys > /dev/null
+# sudo chown reinvent:reinvent /home/reinvent/.ssh/authorized_keys
+# sudo chmod 600 /home/reinvent/.ssh/authorized_keys
 
 echo "Finished!"
 
